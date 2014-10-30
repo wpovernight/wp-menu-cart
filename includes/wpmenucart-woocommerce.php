3bdc6db2a4aa23a47e040c9d2ac477debb246d07
Ewout Fernhout <chocolade@extrapuur.nl>
2014-10-30 11:02:46 +0100

    use get_total() instead of get_cart_total()
    better reflects tax settings

--- includes/wpmenucart-woocommerce.php
+++ includes/wpmenucart-woocommerce.php
@@ -15,7 +15,7 @@ if ( ! class_exists( 'WPMenuCart_WooCommerce' ) ) {
 				'cart_url'				=> $woocommerce->cart->get_cart_url(),
 				'shop_page_url'			=> get_permalink( woocommerce_get_page_id( 'shop' ) ),
 				'cart_contents_count'	=> $woocommerce->cart->get_cart_contents_count(),
-				'cart_total'			=> strip_tags($woocommerce->cart->get_cart_total()),
+				'cart_total'			=> strip_tags($woocommerce->cart->get_total()),
 			);
 		
 			return $menu_item;		
