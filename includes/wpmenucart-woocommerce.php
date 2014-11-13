26da206497986a3ced8b90a3b75570c1a7cf06fb
Ewout Fernhout <chocolade@extrapuur.nl>
2014-11-13 10:33:24 +0100

    final tax tweak!

--- includes/wpmenucart-woocommerce.php
+++ includes/wpmenucart-woocommerce.php
@@ -13,12 +13,13 @@ if ( ! class_exists( 'WPMenuCart_WooCommerce' ) ) {
 			// $woocommerce->cart->get_cart_total() is not a display function,
 			// so we add tax if cart prices are set to display incl. tax
 			// see https://github.com/woothemes/woocommerce/issues/6701
-			if ( !$woocommerce->cart->display_cart_ex_tax ) {
-				$cart_contents_total = woocommerce_price( $woocommerce->cart->cart_contents_total + $woocommerce->cart->tax_total );
-				$cart_contents_total = apply_filters( 'woocommerce_cart_contents_total', $cart_contents_total );
+			if ( $woocommerce->cart->display_cart_ex_tax ) {
+				$cart_contents_total = woocommerce_price( $woocommerce->cart->cart_contents_total );
 			} else {
-				$cart_contents_total = $woocommerce->cart->get_cart_total();
+				$cart_contents_total = woocommerce_price( $woocommerce->cart->cart_contents_total + $woocommerce->cart->tax_total );
 			}
+			$cart_contents_total = apply_filters( 'woocommerce_cart_contents_total', $cart_contents_total );
+
 
 			$menu_item = array(
 				'cart_url'				=> $woocommerce->cart->get_cart_url(),
