00dbd7fbe7d61635a772c977a6bea45bfedc2c68
Ewout Fernhout <info@drwoo.nl>
2020-03-24 17:36:01 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,22 +13,24 @@ VERSION="${TAG//v}"
 
 # Get the SVN data from wp.org in a folder named `svn`
 # svn co --depth immediates "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn
-svn co --depth immediates "https://plugins.svn.wordpress.org/wp-menu-cart" ./wp-svn
-
-svn update --set-depth infinity ./wp-svn/trunk
-svn update --set-depth infinity ./wp-svn/tags/$VERSION
-
-# Copy files from `src` to `svn/trunk`
-rsync -avr --exclude-from=".distignore" ./* ./wp-svn/trunk
+SVN_URL="https://plugins.svn.wordpress.org/wp-menu-cart/"
+SVN_DIR="/github/svn-wp"
+svn checkout --depth immediates "$SVN_URL" "$SVN_DIR"
 
 # Switch to SVN directory
-cd ./wp-svn
+cd "$SVN_DIR"
+
+svn update --set-depth infinity trunk
+svn update --set-depth infinity tags/$VERSION
+
+# Copy files from release to `svn/trunk`
+rsync -avr --exclude-from="$GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/
 
 # Prepare the files for commit in SVN
 svn add --force trunk
 
 # Create the version tag in svn
-svn cp trunk tags/$VERSION
+svn cp "trunk" "tags/$VERSION"
 
 # Prepare the tag for commit
 svn add --force tags
@@ -42,26 +44,28 @@ svn ci  --message "Release $TAG" \
 #### REPEAT FOR woocommerce-menu-bar-cart ####
 
 # Overwrite plugin name
-sed -i -e "s/=== WP Menu Cart ===/=== WooCommerce Menu Cart ===/g" ./readme.txt
-sed -i -e "s/Plugin Name: WP Menu Cart/Plugin Name: WooCommerce Menu Cart/g" ./wp-menu-cart.txt
+sed -i -e "s/=== WP Menu Cart ===/=== WooCommerce Menu Cart ===/g" "$GITHUB_WORKSPACE/readme.txt"
+sed -i -e "s/Plugin Name: WP Menu Cart/Plugin Name: WooCommerce Menu Cart/g" "$GITHUB_WORKSPACE/wp-menu-cart.txt"
 
 # Get the SVN data from wp.org in a folder named `svn`
-svn co --depth immediates "https://plugins.svn.wordpress.org/woocommerce-menu-bar-cart" ./wc-svn
-
-svn update --set-depth infinity ./wc-svn/trunk
-svn update --set-depth infinity ./wc-svn/tags/$VERSION
-
-# Copy files from `src` to `svn/trunk`
-rsync -avr --exclude-from=".distignore" ./* ./wc-svn/trunk
+SVN_URL="https://plugins.svn.wordpress.org/woocommerce-menu-bar-cart/"
+SVN_DIR="/github/svn-wc"
+svn checkout --depth immediates "$SVN_URL" "$SVN_DIR"
 
 # Switch to SVN directory
-cd ./wc-svn
+cd "$SVN_DIR"
+
+svn update --set-depth infinity trunk
+svn update --set-depth infinity tags/$VERSION
+
+# Copy files from release to `svn/trunk`
+rsync -avr --exclude-from="$GITHUB_WORKSPACE/.distignore" "$GITHUB_WORKSPACE/" trunk/
 
 # Prepare the files for commit in SVN
 svn add --force trunk
 
 # Create the version tag in svn
-svn cp trunk tags/$VERSION
+svn cp "trunk" "tags/$VERSION"
 
 # Prepare the tag for commit
 svn add --force tags
