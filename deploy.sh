e11e1ef82332784081bee325f70a6bd960122710
Ewout Fernhout <info@drwoo.nl>
2020-03-24 16:11:00 +0100

    final deployment test

--- deploy.sh
+++ deploy.sh
@@ -14,41 +14,29 @@ VERSION="${TAG//v}"
 
 # Get the SVN data from wp.org in a folder named `svn`
 # svn co --depth immediates "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn
+echo "➤ Checking out .org repository..."
 svn co --depth immediates "https://plugins.svn.wordpress.org/wp-menu-cart" ./svn
 
-touch "./svn/$VERSION.txt";
+svn update --set-depth infinity ./svn/test
+# svn update --set-depth infinity ./svn/tags/$VERSION
+
+# Copy files from `src` to `svn/test`
+rsync -avr --exclude-from=".distignore" ./* ./svn/test
+
+# 3. Switch to SVN directory
 cd ./svn
-svn add --force *
-svn ci  --message "test" \
+
+# Prepare the files for commit in SVN
+svn add --force test
+
+# Create the version tag in svn
+# svn cp trunk tags/$VERSION
+
+# Prepare the tag for commit
+# svn add --force tags
+
+# Commit files to wordpress.org.
+svn ci  --message "Test release $TAG" \
         --username $SVN_USERNAME \
         --password $SVN_PASSWORD \
         --non-interactive
-
-# svn update --set-depth infinity ./svn/trunk
-# svn update --set-depth infinity ./svn/assets
-# svn update --set-depth infinity ./svn/tags/$TAG
-
-# # Copy files from `src` to `svn/trunk`
-# cp -R ./src/* ./svn/trunk
-
-# # Copy the images from `assets` to `svn/assets`
-# cp -R ./assets/* ./svn/assets
-
-# # 3. Switch to SVN directory
-# cd ./svn
-
-# # Prepare the files for commit in SVN
-# svn add --force trunk
-# svn add --force assets
-
-# # Create the version tag in svn
-# svn cp trunk tags/$TAG
-
-# # Prepare the tag for commit
-# svn add --force tags
-
-# # Commit files to wordpress.org.
-# svn ci  --message "Release $TAG" \
-#         --username $SVN_USERNAME \
-#         --password $SVN_PASSWORD \
-#         --non-interactive
