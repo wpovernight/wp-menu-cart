5ee4578f4756a40dabda365d994c89e8448b7f54
Yordan Soares <38109855+YordanSoares@users.noreply.github.com>
2024-03-06 17:18:53 +0100

    Tweak: add explicit labels for the disabling options for the e-commerce plugin and menus (#50)

--- includes/wpmenucart-settings.php
+++ includes/wpmenucart-settings.php
@@ -657,7 +657,9 @@ class WpMenuCart_Settings {
 		$disabled = (isset( $args['disabled'] )) ? ' disabled' : '';
 		
 		$html  = sprintf( '<select name="%1$s[%2$s]" id="%1$s[%2$s]"%3$s>', esc_attr( $menu ), esc_attr( $id ), esc_attr( $disabled ) );
-		$html .= sprintf( '<option value="%s"%s>%s</option>', '0', selected( $current, '0', false ), '' );
+		if ( 'shop_plugin' === $args['id'] ) {
+			$html .= sprintf( '<option value="">%s</option>', __( 'Select a choice…', 'wp-menu-cart' ) );
+		}
 		
 		foreach ( $args['options'] as $key => $label ) {
 			$html .= sprintf( '<option value="%s"%s>%s</option>', esc_attr( $key ), selected( $current , $key, false ), esc_attr( $label ) );
@@ -700,7 +702,7 @@ class WpMenuCart_Settings {
 			$disabled = ($x == 1) ? '' : ' disabled';
 			
 			$html .= sprintf( '<select name="%1$s[%2$s][%3$s]" id="%1$s[%2$s][%3$s]"%4$s>', esc_attr( $menu ), esc_attr( $id ), esc_attr( $x ), esc_attr( $disabled ) );
-			$html .= sprintf( '<option value="%s"%s>%s</option>', '0', selected( $current, '0', false ), '' );
+			$html .= sprintf( '<option value="">%s</option>', __( 'Select a choice…', 'wp-menu-cart' ) );
 			
 			foreach ( (array) $args['options'] as $key => $label ) {
 				$html .= sprintf( '<option value="%s"%s>%s</option>', $key, selected( $current, $key, false ), esc_attr( $label ) );
