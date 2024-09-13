70274c2cfc53a54eb5ca77d38889eb4a119f2d20
Mohamad <136313810+MohamadNateqi@users.noreply.github.com>
2024-09-13 14:41:32 +0330

    Fix: issue with using `wc_get_cart_url()` in Woo version 9.3 (#56)

--- includes/wpmenucart-edd.php
+++ includes/wpmenucart-edd.php
@@ -2,10 +2,8 @@
 if ( ! class_exists( 'WPMenuCart_EDD' ) ) {
 	class WPMenuCart_EDD {	
 		public function menu_item() {
-			global $post;
-	
 			$menu_item = array(
-				'cart_url'            => edd_get_checkout_uri(),
+				'cart_url'            => apply_filters( 'wpmenucart_cart_url', edd_get_checkout_uri(), $this ),
 				'shop_page_url'       => get_home_url(),
 				'cart_contents_count' => edd_get_cart_quantity(),
 				'cart_total'          => edd_currency_filter( edd_format_amount( edd_get_cart_total() ) ),
--- includes/wpmenucart-woocommerce.php
+++ includes/wpmenucart-woocommerce.php
@@ -11,7 +11,7 @@ if ( ! class_exists( 'WPMenuCart_WooCommerce' ) ) {
 			$this->maybe_load_cart(); // make sure cart is loaded! https://wordpress.org/support/topic/activation-breaks-customise?replies=10#post-7908988
 
 			$menu_item = array(
-				'cart_url'            => wc_get_cart_url(),
+				'cart_url'            => $this->get_cart_url(),
 				'shop_page_url'       => wc_get_page_permalink( 'shop' ),
 				'cart_total'          => strip_tags( $this->get_cart_total() ),
 				'cart_contents_count' => $this->get_cart_contents_count(),
@@ -90,6 +90,23 @@ if ( ! class_exists( 'WPMenuCart_WooCommerce' ) ) {
 			}
 
 			return $cart_contents_total;
+		}
+
+		/**
+		 * Get the cart URL.
+		 *
+		 * Taking into account the changes made to `wc_get_cart_url()` in WooCommerce version 9.3.0,
+		 * the `wc_get_cart_url()` may return the current URL during AJAX requests,
+		 * which could result in incorrect URLs like `/?wc-ajax=add_to_cart`.
+		 *
+		 * To ensure the correct cart page URL is returned, the `wc_get_page_permalink( 'cart' )`
+		 * has been used instead, with a filter to allow further customizations.
+		 *
+		 * @return string The filtered cart URL.
+		 */
+		public function get_cart_url(): string {
+			$wc_cart_url = apply_filters( 'woocommerce_get_cart_url', wc_get_page_permalink( 'cart' ) );
+			return apply_filters( 'wpmenucart_cart_url', $wc_cart_url, $this );
 		}
 	}
 }
